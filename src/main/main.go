74433954e08270c74ed2fcc0e146942e2f5f46ad
Liu <windy.liu@honeywell.com>
2016-07-04 09:27:18 +0800

    Remove code to xmlCOnfig package

--- src/main/main.go
+++ src/main/main.go
@@ -3,23 +3,21 @@ package main
 //import protocol "protocol"
 
 import (
-	"bytes"
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"net"
 	"os"
 
 	"../protocol"
+	"../xmlConfig"
 )
 
 func main() {
 
 	var configMap map[string]string
-	configMap = getConfig()
+	configMap = xmlConfig.GetConfig()
 
-	localhost := getElement("host", configMap)
-	port := getElement("port", configMap)
+	localhost := xmlConfig.GetElement("host", configMap)
+	port := xmlConfig.GetElement("port", configMap)
 
 	//建立socket，监听端口
 	netListen, err := net.Listen("tcp", localhost+":"+port)
@@ -82,58 +80,4 @@ func CheckError(err error) {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
 	}
-}
-
-//Get Host address and prot frm configuration file.
-func getConfig() map[string]string {
-
-	var token xml.Token
-	var err error
-
-	configMap := make(map[string]string)
-
-	currentPath, err := os.Getwd()
-	CheckError(err)
-
-	configByte, err := ioutil.ReadFile(currentPath + "/config.xml")
-
-	decoder := xml.NewDecoder(bytes.NewBuffer(configByte))
-
-	var elementCount int
-
-	var keyList []string
-	var valueList []string
-
-	for token, err = decoder.Token(); err == nil; token, err = decoder.Token() {
-
-		switch token := token.(type) {
-		// 处理元素
-		case xml.StartElement:
-			name := token.Name.Local
-			keyList = append(keyList, name)
-			fmt.Println(keyList)
-			elementCount++
-		case xml.CharData:
-			content := string([]byte(token))
-			if content != "\n" {
-				valueList = append(valueList, content)
-				fmt.Println(valueList)
-			}
-
-		}
-	}
-	for index := 0; index < elementCount; index++ {
-		configMap[keyList[index]] = valueList[index]
-	}
-	fmt.Println(elementCount)
-
-	return configMap
-}
-
-func getElement(key string, themap map[string]string) string {
-	if value, ok := themap[key]; ok {
-		return value
-	}
-
-	return ""
 }
